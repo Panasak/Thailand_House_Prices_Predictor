cf835dba5331d5edc9de9a52cb5e77f292889031
Panasak <89185433+Panasak@users.noreply.github.com>
2021-12-09 08:50:09 +0100

    new location

---
+++ model_building/model_build.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  9 08:38:35 2021
+
+@author: tango
+"""
+import pandas as pd
+import numpy as np
+
+pd.read_csv('thai_housing_price_data_cleaned.csv')
